b47e7c627cd7d54b7092a53c9b54978a02e0e1bd
Talace <0x.88ta.ko@gmail.com>
2023-11-12 12:41:31 +0100

    ADD delete function

--- src/main.py
+++ src/main.py
@@ -51,6 +51,46 @@ class Autolog:
                 for category, user_info in self.users.items():
                     print(f"{category}: {user_info['name']} - {user_info['email']}")
 
+    def login(self):
+        category = input("[+] Enter the name of the login category you want to use: ")
+        if not os.path.exists(self.data_yaml):
+            print("[-] No file or no USER found.")
+
+        else:
+            with open(self.data_yaml, 'r') as data_file:
+                self.users = yaml.safe_load(data_file)
+
+            if category in self.users:
+                print(f"[+] Logging in as {self.users[category]['name']}")
+                os.system(f"git config --global user.name {self.users[category]['name']}")
+                os.system(f"git config --global user.email {self.users[category]['email']}")
+            else:
+                print(f"[-] Login category '{category}' not found.")
+
+    def delete(self):
+        category = input("[+] Enter the name of the login category you want to DELETE: ")
+
+        if not os.path.exists(self.data_yaml):
+            print("[-] No file or no USER found.")
+            return
+
+        with open(self.data_yaml, 'r') as data_file:
+            self.users = yaml.safe_load(data_file)
+
+        if category in self.users:
+            choice = input(
+                f"[?] Are you sure you want to delete user '{self.users[category]['name']}' with email '{self.users[category]['email']}'? (y/n):")
+            if choice.lower() == 'y':
+                del self.users[category]
+                with open(self.data_yaml, 'w') as data:
+                    yaml.dump(self.users, data, default_style='\'"')
+                print(f"[+] Login category '{category}' deleted.")
+            else:
+                print("[+] Deletion canceled.")
+                print("[+] Existing, bye!")
+        else:
+            print(f"[-] Login category '{category}' not found.")
+
     def setup_parser(self):
         self.parser = argparse.ArgumentParser(description="Command-line tool for GitHub config global")
         self.parser.add_argument('-c', '--create', action="store_true", help="Create something")
