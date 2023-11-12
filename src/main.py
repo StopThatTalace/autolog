c7b760427aa4f72d0e36426474f47a745db4838d
Talace <0x.88ta.ko@gmail.com>
2023-11-12 11:37:47 +0100

    UPDATE class name

--- src/main.py
+++ src/main.py
@@ -3,10 +3,11 @@ import yaml
 
 data_yaml = "data.yaml"
 
-class GithubConfigTool:
+class Autolog:
     def __init__(self, data_yaml="data.yaml"):
         self.data_yaml = data_yaml
         self.users = {}
 
 if __name__ == "__main__":
-    github_tool = GithubConfigTool()
+    autolog = Autolog()
+    autolog.main()
